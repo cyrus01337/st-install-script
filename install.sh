e3206188b1ef5e5406d4d757d43d79008b61a6cb
cyrus01337 <cyrus01337@gmail.com>
2025-06-08 15:47:02 +0100

    Include other variation of script

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ set -e
 INSTALLATION_DIRECTORY="/opt/st"
 CONFIGURATION_FILE="$INSTALLATION_DIRECTORY/config.def.h"
 
-sudo mkdir --from "$USER:$USER" --mode 711 $INSTALLATION_DIRECTORY || true
-git clone --single-branch --depth 1 https://git.suckless.org/st $INSTALLATION_DIRECTORY
+sudo mkdir --mode 711 $INSTALLATION_DIRECTORY || true
+sudo chown -R "$USER:$USER" $INSTALLATION_DIRECTORY && \
+    git clone --single-branch --depth 1 https://git.suckless.org/st $INSTALLATION_DIRECTORY
 sed -i "s/Liberation Mono:pixel/FantasqueSansM Nerd Font:/" $CONFIGURATION_FILE && \
     sed -i "s/\/bin\/sh/\/usr\/bin\/bash/" $CONFIGURATION_FILE && \
     sed -i -E "s/(tabspaces = )8/\14/" $CONFIGURATION_FILE
+cp -r patches/ $INSTALLATION_DIRECTORY && \
+    cd $INSTALLATION_DIRECTORY
 
 for patch in patches/*.diff; do
     echo ""
